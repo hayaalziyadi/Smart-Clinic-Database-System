7aa8f33b70b6157ff4173b38c0d99ddca8f68ace
hayaalziyadi <hayaalziyadi2@gmail.com>
2026-07-31 20:38:26 +0300

    feat:add data insertions for tables

---
+++ smartclinicDbInsertData.sql
@@ -0,0 +1,6 @@
+INSERT INTO Payments (AppointmentID, PaymentDate, AmountPaid, PaymentMethod) VALUES
+(1, '2026-08-01', 250.00, 'Credit Card'),
+(2, '2026-08-01', 150.00, 'Cash'),
+(3, '2026-08-02', 200.00, 'Credit Card'),
+(4, '2026-08-02', 180.00, 'Insurance'),
+(5, '2026-08-03', 0.00, 'Cash');
